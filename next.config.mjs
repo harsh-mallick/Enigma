6c0ce31dfc331fdc5fe448fcd44e4e4717ac0d2a
Harsh Mallick <89124261+harsh-mallick@users.noreply.github.com>
2024-11-11 21:03:30 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        AppName: 'Enigma',
-        MONGO_URL: "mongodb+srv://admin:admin@enigma%23123@cluster0.sajmo.mongodb.net/"
+        AppName: 'Enigma'
     }
 };
 
